3a0c7c6dc7520071c84c8c189f6c0d0d285e5825
Elvijs <e.michulis@gmail.com>
2024-09-06 18:33:26 +0300

    added startup, added basic weather data timer function

---
+++ Task1/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+[assembly: FunctionsStartup(typeof(Task1.Startup))]
+
+namespace Task1;
+
+public class Startup : FunctionsStartup
+{
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        builder.Services.AddHttpClient();
+        builder.Services.AddSingleton(_ => new BlobServiceClient(Environment.GetEnvironmentVariable("AzureWebJobsStorage")));
+    }
+}
